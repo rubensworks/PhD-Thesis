d3b4a28a03860274f78c5f74724387d954905959
Ruben Taelman <ruben.taelman@ugent.be>
2019-08-27 11:43:19 +0200

    Add make-pdf tool

---
+++ make-pdf.sh
@@ -0,0 +1,3 @@
+chrome-headless-render-pdf --url http://localhost:3000/ --pdf article.pdf --paper-width 8.27 --paper-height 11.69 --no-margins \
+#    --display-header-footer --header-template ' ' \
+#    --footer-template '<style type="text/css">.footer{font-size:8px;width:100%;text-align:right;color:#000;padding:0.65cm;}</style><div class="footer"><span class="pageNumber"></span> / <span class="totalPages"></span></div>'
